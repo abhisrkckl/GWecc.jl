8d4deb8effe7a39124d4f4d91128df74598bca1c
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2022-12-05 13:40:01 -0600

    hpx_spx example

--- examples/hpx_spx.jl
+++ examples/hpx_spx.jl
@@ -1,4 +1,4 @@
-"""Comparison of eccentric waveforms (+/x polarizations) for different eccentricities.
+"""Comparison of eccentric waveforms and residuals (+/x polarizations) for different eccentricities.
 """
 
 include("../src/GWecc.jl")
@@ -20,14 +20,26 @@ ts = Time.(LinRange(0, 10 * year, 5000))
 
 for (idx, e_init) in enumerate(Eccentricity.([0.1, 0.4, 0.8]))
     coeffs = EvolvCoeffs(mass, n_init, e_init)
+    
     hpxs = [waveform_hpx(mass, coeffs, l_init, proj, dl, false, dt) for dt in ts]
     hps = [hpx[1] for hpx in hpxs]
     hxs = [hpx[2] for hpx in hpxs]
-
-    subplot(310 + idx)
+    
+    subplot(320 + 2*idx - 1)
     plot([t.t for t in ts]/year, hps)
     plot([t.t for t in ts]/year, hxs)
     ylabel("\$h_{+,\\times}\$")
+    xlabel("t (year)")
+
+    spxs = [residual_spx(mass, coeffs, l_init, proj, dl, false, dt) for dt in ts]
+    sps = [spx[1] for spx in spxs]
+    sxs = [spx[2] for spx in spxs]
+
+    subplot(320 + 2*idx)
+    plot([t.t for t in ts]/year, sps)
+    plot([t.t for t in ts]/year, sxs)
+    ylabel("\$s_{+,\\times}\$")
+    xlabel("t (year)")
 end
-xlabel("t (year)")
+
 show()
