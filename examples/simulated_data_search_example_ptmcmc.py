2765d233b25da6d83bad6e97636ce9f8b8c89d55
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-01-30 12:59:47 -0600

    black

--- examples/simulated_data_search_example_ptmcmc.py
+++ examples/simulated_data_search_example_ptmcmc.py
@@ -21,15 +21,15 @@ name = "gwecc"
 tref = max(psr.toas)
 priors = {
     "alpha": true_params["alpha"],  # Uniform(0, 1)(f"{name}_alpha"),
-    "psi": true_params["psi"], # Uniform(0, np.pi)(f"{name}_psi"),
-    "cos_inc": true_params["cos_inc"], # Uniform(-1, 1)(f"{name}_cos_inc"),
+    "psi": true_params["psi"],  # Uniform(0, np.pi)(f"{name}_psi"),
+    "cos_inc": true_params["cos_inc"],  # Uniform(-1, 1)(f"{name}_cos_inc"),
     "log10_M": true_params["log10_M"],  # Uniform(6, 9)(f"{name}_log10_M"),
     "eta": true_params["eta"],  # Uniform(0, 0.25)(f"{name}_eta"),
     "log10_F": true_params["log10_F"],  # Uniform(-9, -7)(f"{name}_log10_F"),
     "e0": true_params["e0"],  # Uniform(0.01, 0.8)(f"{name}_e0"),
-    "gamma0": true_params["gamma0"], # Uniform(0, np.pi)(f"{name}_gamma0"),
+    "gamma0": true_params["gamma0"],  # Uniform(0, np.pi)(f"{name}_gamma0"),
     "gammap": 0.0,  # Uniform(0, np.pi),
-    "l0": true_params["l0"], # Uniform(0, 2 * np.pi)(f"{name}_l0"),
+    "l0": true_params["l0"],  # Uniform(0, 2 * np.pi)(f"{name}_l0"),
     "lp": 0.0,  # Uniform(0, 2 * np.pi),
     "tref": tref,
     "log10_zc": Uniform(-5, -3)(f"{name}_log10_zc"),
@@ -50,21 +50,23 @@ ndim = len(x0)
 cov = np.diag(np.ones(ndim) * 0.01**2)
 Nsteps = 10000
 x0 = np.hstack(x0)
-sampler = ptmcmc(ndim, pta.get_lnlikelihood, pta.get_lnprior, cov, outDir="gwecc_sims/chains/")
+sampler = ptmcmc(
+    ndim, pta.get_lnlikelihood, pta.get_lnprior, cov, outDir="gwecc_sims/chains/"
+)
 sampler.sample(x0, Nsteps)
 
 chain_file = "gwecc_sims/chains/chain_1.txt"
 chain = np.loadtxt(chain_file)
 
 plt.subplot(121)
-param_name = pta.param_names[0][len("gwecc_"):]
-plt.plot(chain[:,:-4][:,0])
+param_name = pta.param_names[0][len("gwecc_") :]
+plt.plot(chain[:, :-4][:, 0])
 plt.axhline(true_params[param_name], c="k")
 plt.ylabel(param_name)
 
 plt.subplot(122)
 burn = 50
-burned_chain = chain[burn:,:-4][:,0]
+burned_chain = chain[burn:, :-4][:, 0]
 plt.hist(burned_chain, bins=16)
 plt.axvline(true_params[param_name], c="k")
 plt.xlabel(param_name)
