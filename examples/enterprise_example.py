09280d79d31292e3acb65933cd0f6d31900ba519
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-02-24 10:53:18 -0600

    fix example

--- examples/enterprise_example.py
+++ examples/enterprise_example.py
@@ -6,7 +6,7 @@ from enterprise.pulsar import Pulsar
 from enterprise.signals.gp_signals import MarginalizingTimingModel
 from enterprise.signals.signal_base import PTA
 
-from enterprise_gwecc import eccentric_pta_signal_planck18_1psr, gwecc_1psr_block
+from enterprise_gwecc import eccentric_pta_signal_1psr, gwecc_1psr_block
 
 # Calling eccentric_pta_signal_planck18_1psr
 year = 365.25 * 24 * 3600
@@ -22,8 +22,8 @@ e0 = 0.3
 gamma0 = gammap = 0.0
 l0 = lp = 0.0
 tref = max(toas)
-log10_zc = -2.0
-res = eccentric_pta_signal_planck18_1psr(
+log10_dl = -15
+res = eccentric_pta_signal_1psr(
     toas,
     pdist,
     alpha,
@@ -38,7 +38,7 @@ res = eccentric_pta_signal_planck18_1psr(
     l0,
     lp,
     tref,
-    log10_zc,
+    log10_dl,
 )
 plt.plot(toas, res)
 plt.show()
@@ -46,9 +46,9 @@ plt.show()
 
 # Creating a Signal object with gwecc_1psr_block
 psr = Pulsar(
-    "data/J1909-3744_NANOGrav_12yv4.gls.par",
-    "data/J1909-3744_NANOGrav_12yv4.tim",
-    timing_package="pint",
+    "data/JPSR00_simulate.par",
+    "data/JPSR00_simulate.tim",
+    timing_package="tempo2",
 )
 tm = MarginalizingTimingModel()
 ecw = gwecc_1psr_block(tref=max(psr.toas))
