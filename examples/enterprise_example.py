5971df5fd31ebb5cd98e26cceac748807c59e4e7
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-03-01 10:28:50 -0600

    fix enterprise_example.py

--- examples/enterprise_example.py
+++ examples/enterprise_example.py
@@ -11,34 +11,28 @@ from enterprise_gwecc import eccentric_pta_signal_1psr, gwecc_1psr_block
 # Calling eccentric_pta_signal_planck18_1psr
 year = 365.25 * 24 * 3600
 toas = np.linspace(0, 5 * year, 100)
-pdist = 1.5
-alpha = 0.3
-psi = 1.2
-cos_inc = 0.5
+sigma = 1.2
+rho = 0.5
 log10_M = 8.0
 eta = 0.2
 log10_F = -8.0
 e0 = 0.3
-gamma0 = gammap = 0.0
-l0 = lp = 0.0
+l0 = 0.0
 tref = max(toas)
 log10_A = -9.0
+deltap = 100*year
 res = eccentric_pta_signal_1psr(
     toas,
-    pdist,
-    alpha,
-    psi,
-    cos_inc,
+    sigma,
+    rho,
     log10_M,
     eta,
     log10_F,
     e0,
-    gamma0,
-    gammap,
     l0,
-    lp,
     tref,
     log10_A,
+    deltap
 )
 plt.plot(toas, res)
 plt.show()
