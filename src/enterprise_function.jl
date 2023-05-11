c5d750a655dae34db0bef98f1db355b19ce63ac2
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2023-05-11 14:35:21 -0500

    eccentric_pta_signal_components

--- src/enterprise_function.jl
+++ src/enterprise_function.jl
@@ -2,7 +2,8 @@ export eccentric_pta_signal,
     eccentric_pta_signal_1psr,
     eccentric_pta_signal_target,
     validate_params,
-    validate_params_target
+    validate_params_target,
+    eccentric_pta_signal_components
 
 "High-level interface for the residuals function. This function computes 
 the eccentric PTA signal for a single pulsar given a collection of TOAs 
@@ -244,4 +245,44 @@ function validate_params_target(
     end
 
     return true
+end
+
+function eccentric_pta_signal_components(
+    toas,
+    theta::Float64,
+    phi::Float64,
+    pdist::Float64,
+    cos_gwtheta::Float64,
+    gwphi::Float64,
+    log10_M::Float64,
+    eta::Float64,
+    log10_F::Float64,
+    e0::Float64,
+    l0::Float64,
+    lp::Float64,
+    tref::Float64,
+    psrTerm::Bool = false,
+)
+    mass = mass_from_log10_mass(log10_M, eta)
+
+    n_init = mean_motion_from_log10_freq(log10_F)
+    e_init = Eccentricity(e0)
+
+    l0p = InitPhaseParams(l0, lp)
+
+    dp = psrdist_from_pdist(pdist)
+
+    ra_psr = phi
+    dec_psr = π / 2 - theta
+    psrpos = SkyLocation(ra_psr, dec_psr)
+
+    ra_gw = gwphi
+    dec_gw = asin(cos_gwtheta)
+    gwpos = SkyLocation(ra_gw, dec_gw)
+
+    term = psrTerm ? PULSAR : EARTH
+    tref = Time(tref)
+    tEs = Time.(toas)
+
+    return residuals_components_𝒜(mass, n_init, e_init, l0p, dp, psrpos, gwpos, term, tref, tEs)
 end
