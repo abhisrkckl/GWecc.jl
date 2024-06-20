7cd616b7d7d6feef8421376c83d7ff5430f5002b
Abhimanyu Susobhanan <abhisrkckl@gmail.com>
2024-06-20 16:02:20 +0200

    tau_vs_e

--- examples/tau_vs_e.jl
+++ examples/tau_vs_e.jl
@@ -2,14 +2,16 @@
 Figure 2 of Susobhanan+ 2020"""
 
 using GWecc
-using PyPlot
+using CairoMakie
 
 println("Running ", PROGRAM_FILE)
 
 es = Eccentricity.(LinRange(0.01, 0.99, 1000))
 τs = τ_from_e.(es)
 
-plot([τ.τ for τ in τs], [e.e for e in es])
-xlabel("τ")
-ylabel("e")
-show()
+fig = Figure()
+ax = Axis(fig[1,1])
+CairoMakie.lines!([τ.τ for τ in τs], [e.e for e in es])
+ax.xlabel = "τ"
+ax.ylabel = "e"
+save("tau_vs_e.pdf", fig)
